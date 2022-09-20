026fae455954e81bb875cbe1e4dd50f12ca78e40
alejooroncoy <adoa2705@gmail.com>
2022-09-19 22:52:59 -0500

    Update movies component ⚛🎥👨‍💻

--- src/utils/loadMovies.js
+++ src/utils/loadMovies.js
@@ -1,6 +1,6 @@
 import { optionsFetch } from "../config/optionsFetch";
 
-export default async function loadMovies(page = 1, language = 'en-US', amount = 10) {
+export default async function loadMovies(page = 1, language = 'en-US') {
   const baseURL = 'https://api.themoviedb.org/3';
   const URI = `/discover/movie?page=${page}&language=${language}`;
   const response = await fetch(baseURL.concat(URI), optionsFetch);
